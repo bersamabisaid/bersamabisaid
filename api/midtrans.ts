107814cb3e1fc0994a302b3caaed2d4f89fb19a5
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-03-14 02:02:51 +0700

    refactor(api): :alien: use API (axios) instead of midtrans-node-client library

--- api/midtrans.ts
+++ api/midtrans.ts
@@ -1,24 +1,51 @@
-import { NowApiHandler } from '@vercel/node';
-import { MidtransClient } from 'midtrans-node-client';
+/* eslint-disable camelcase */
+import axios, { AxiosError } from 'axios';
+import type { NowApiHandler } from '@vercel/node';
 import allowCors from './utils/allowCors';
 
-const snap = new MidtransClient.Snap({
-  isProduction: false,
-  serverKey: process.env.MIDTRANS_serverKey,
-  clientKey: process.env.MIDTRANS_clientKey,
-});
+const API_URL = {
+  production: 'https://app.midtrans.com/snap/v1/transactions',
+  dev: 'https://app.sandbox.midtrans.com/snap/v1/transactions',
+};
+
+interface SuccessResponse {
+  token: string;
+  redirect_url: string;
+}
+
+interface ErrorResponse {
+  error_messages: Array<string>;
+}
+
+const encodeBase64 = (v: string) => Buffer.from(v).toString('base64');
+
+const authString = encodeBase64(`${process.env.MIDTRANS_serverKey || ''}:`);
 
 module.exports = allowCors((async (req, res) => {
-  const transaction = await snap.createTransactionRedirectUrl({
-    transaction_details: {
-      order_id: Date.now(),
-      gross_amount: 1,
-    },
-  });
+  try {
+    const transaction = await axios.post<SuccessResponse>(
+      API_URL.dev,
+      {
+        transaction_details: {
+          order_id: Date.now(),
+          gross_amount: 1,
+        },
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Basic ${authString}`,
+        },
+      },
+    );
 
-  if (req.headers.accept === 'application/json') {
-    res.json(transaction);
-  } else {
-    res.redirect(transaction);
+    if (req.headers.accept === 'application/json') res.json(transaction.data);
+    else res.redirect(transaction.data.redirect_url);
+  } catch (err) {
+    const {
+      response,
+    } = err as AxiosError<ErrorResponse>;
+
+    res.json(response?.data || { error: true });
   }
 }) as NowApiHandler);
