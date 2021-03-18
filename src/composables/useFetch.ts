4491fd5fd43ebe695213e290579f81c3738735e6
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-03-19 00:43:08 +0700

    feat: :sparkles: useFetch: disable auto fetch onMount

--- src/composables/useFetch.ts
+++ src/composables/useFetch.ts
@@ -1,15 +1,39 @@
-import { onMounted, ref, UnwrapRef } from '@vue/composition-api';
+import {
+  onMounted, ref, Ref, UnwrapRef,
+} from '@vue/composition-api';
 
-export default function useFetch<T>(initialValue: T, ...args: Parameters<typeof fetch>) {
+type IuseFetch = <T> (
+  initialValue: T,
+  input: RequestInfo,
+  init?: RequestInit | undefined,
+) => Readonly<[
+  Ref<UnwrapRef<T>>,
+  Ref<boolean>,
+  () => Promise<void>
+]>;
+
+const useFetch: IuseFetch = function (initialValue, input, init) {
   const data = ref(initialValue);
+  const isLoading = ref(true);
+  const fetcher = async function () {
+    isLoading.value = true;
+    const res = await fetch(input, init);
 
-  onMounted(() => (async () => {
-    const res = await fetch(...args);
+    isLoading.value = false;
+    data.value = await res.json() as UnwrapRef<typeof initialValue>;
+  };
 
-    data.value = (await res.json()) as UnwrapRef<T>;
-  })());
+  return [data, isLoading, fetcher];
+};
 
-  return [
-    data,
-  ] as const;
-}
+const onMount: IuseFetch = function (initialValue, input, init) {
+  const [data, isLoading, fetcher] = useFetch(initialValue, input, init);
+
+  onMounted(() => fetcher());
+
+  return [data, isLoading, fetcher];
+};
+
+export default Object.assign(useFetch, {
+  onMount,
+});
