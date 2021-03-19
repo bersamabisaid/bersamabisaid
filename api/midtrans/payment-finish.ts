a9bef57ec45150ef5d2069e594bf281e7eb0ab11
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-03-20 02:25:16 +0700

    feat(api): :sparkles: midtrans/payment-finish done!

--- api/midtrans/payment-finish.ts
+++ api/midtrans/payment-finish.ts
@@ -1,9 +1,29 @@
-import { NowApiHandler } from '@vercel/node';
-import allowCors from '../utils/_allowCors';
-import apiMethod from '../utils/_apiMethod';
+import hasRequiredQuery from '../_middleware/hasRequiredQuery';
+import allowCors from '../_middleware/allowCors';
+import apiMethod from '../_middleware/apiMethod';
+import { collection } from '../_services/firebase';
+import { FinishPaymentRedirectQuery } from '../../shared/types/midtransApi';
 
-const handler: NowApiHandler = (req, res) => {
-  res.end();
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable camelcase */
+const isFinishPaymentRedirectQuery = function (data: any): data is FinishPaymentRedirectQuery {
+  return Boolean(
+    data?.order_id,
+  );
 };
+/* eslint-enable @typescript-eslint/no-unsafe-member-access */
+/* eslint-enable camelcase */
+
+const handler = hasRequiredQuery(isFinishPaymentRedirectQuery, async ({ query }, res) => {
+  const doc = await collection.Donations.doc(query.order_id).get();
+
+  if (doc.exists) {
+    const data = doc.data();
+
+    res.redirect(data!._system.finishPaymentRedirectUrl);
+  }
+
+  res.status(404).end();
+});
 
 module.exports = allowCors(apiMethod.get(handler));
