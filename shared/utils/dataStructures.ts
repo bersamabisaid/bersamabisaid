28cd0a12392c980038de00c975caa49176665310
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-04-21 08:22:14 +0700

    feat(shared): :sparkles: add dataStructures utils

---
+++ shared/utils/dataStructures.ts
@@ -0,0 +1,31 @@
+interface QueueConstructorOption<T> {
+  maxSize: number;
+  initialValue?: T[];
+}
+
+export class Queue<T> {
+  #value: T[] = [];
+
+  maxSize: number;
+
+  constructor({ maxSize, initialValue = [] }: QueueConstructorOption<T>) {
+    this.maxSize = maxSize;
+    initialValue.forEach((data) => this.append(data));
+  }
+
+  append(data: T) {
+    if (this.#value.length > this.maxSize) {
+      this.#value.shift();
+    }
+
+    this.#value.push(data);
+  }
+
+  pop() {
+    this.#value.pop();
+  }
+
+  get() {
+    return this.#value;
+  }
+}
