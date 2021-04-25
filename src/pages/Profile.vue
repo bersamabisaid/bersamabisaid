1e8a0a1b0cf0dd60a6db302c22767a36932c0c2b
Abyanzhafran <52287739+Abyanzhafran@users.noreply.github.com>
2021-04-26 03:30:24 +0700

    feat(page): :lipstick: update section

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -170,13 +170,13 @@
               Tim Yang Kita Miliki
             </h6>
           </div>
-          <div class="py-9 flex justify-center gap-16">
-            <q-card
+          <div class="py-9 flex justify-center gap-8">
+            <!-- <q-card
               v-for="i in 5"
               :key="i"
               flat
               bordered
-              class="my-card bg-grey-1"
+              class="my-card bg-grey-1 outline-none"
             >
               <q-card-section class="bg-blue-400">
                 <div class="row items-center no-wrap">
@@ -220,6 +220,66 @@
 
               <q-card-section>
                 {{ lorem }}
+              </q-card-section>
+            </q-card> -->
+            <div
+              v-for="i in 5"
+              :key="i"
+              class="max-w-xs mx-auto bg-white rounded-lg shadow-lg dark:bg-gray-800 mt-20"
+            >
+              <div class="flex justify-center -mt-16 md:justify-end">
+                <img
+                  class="object-cover w-20 h-20 border-2 border-indigo-500 rounded-full dark:border-indigo-400"
+                  alt="Testimonial avatar"
+                  src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"
+                >
+              </div>
+              <div class="px-4 py-2">
+                <h1 class="text-3xl font-bold text-gray-800 uppercase dark:text-white">
+                  SOCIAL EDUCATION DEVELOPMENT
+                </h1>
+              </div>
+
+              <div class="px-4 py-2">
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                  {{ lorem }}
+                </p>
+              </div>
+
+              <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+                <h1 class="text-lg font-bold text-white">
+                  Lorem aja
+                </h1>
+                <button class="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-200 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
+                  Lorem lagi
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="relative pt-4 pb-10">
+        <div class="container relative mx-auto px-4 flex flex-col">
+          <div class="flex justify-center pt-8">
+            <q-card class="bg-grey rounded-2xl">
+              <q-card-section class="m-8">
+                <h6 class="text-3xl font-bold text-gray-800 dark:text-gray-100 mb-6">
+                  Atas apa yang telah kita lakukan bersama, kita berharap
+                </h6>
+                <p>
+                  1. Meningkatnya angka orientasi pendidikan masyarakat
+                  kurang mampu
+                </p>
+                <p>
+                  2. Meningkatnya akses belajar melalui program yang dilaksanakan
+                </p>
+                <p>
+                  3. Tersedianya fasilitas belajar untuk daerah yang membutuhkan
+                  dan prioritas
+                </p>
+                <p>
+                  4. Masyarakat kurang mampu tidak khawatir dengan pendidikan anaknya
+                </p>
               </q-card-section>
             </q-card>
           </div>
